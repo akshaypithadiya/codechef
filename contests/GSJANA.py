57d2e23e3cc282986d51bd419dfa9690a571619c
akshaypithadiya <akshay.pithadiya@gmail.com>
2018-01-21 22:01:27 +0530

    solved contests problems

---
+++ contests/GSJANA.py
@@ -0,0 +1,11 @@
+tc = int(input())
+
+for x in range(tc):
+	r,n,s,e,w = map(int,input().split())
+	lst = [r,n,s,e,w]
+	if lst.count(r)>=2:
+		print("YES")
+	else:
+		print("NO")
+
+#solved
---
+++ contests/QM6A.py
@@ -0,0 +1,21 @@
+tc = int(input())
+
+for x in range (tc):
+	n = int(input())
+
+	lst=[]
+	rlst=[]
+
+	for i in range(1,n+1):
+		lst.append(i)
+		rlst.append(i)
+
+	rlst.pop()
+	rlst.reverse()
+	nlst = lst + rlst
+
+	sqlst = [z**2 for z in nlst]
+	s = sum(sqlst)
+	print(s)
+
+	#solved
